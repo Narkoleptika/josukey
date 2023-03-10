3166630424065ed44ec5d2bac2036d4fc8ae94dc
Joey <joe@joey.codes>
2023-03-09 19:33:41 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Josukey
+
+A keyboard for Joey
+
+## Getting Started
+
+Get my files on your computer 😏 and install dependencies.
+
+```bash
+git clone https://github.com/Narkoleptika/josukey.git
+cd josukey
+npm i
+```
+
+## Build
+
+This will run ergogen and build all of the output files.
+
+```bash
+npm run build
+```
+
+## Watch
+
+This will watch the `config.yaml` file and the `footprints` directory and run the build command whenever there are changes.
+
+```bash
+npm run watch
+```
---
+++ footprints/wuerth_434121025816.js
@@ -0,0 +1,54 @@
+module.exports = {
+    params: {
+        designator: 'S',
+        reverse: true,
+        r1: { type: 'net', value: 'r1' },
+        r2: { type: 'net', value: 'r2' },
+    },
+    body: p => {
+        const standard = `
+            (footprint 434121025816 (layer F.Cu) (tedit 63B6D2D4)
+            ${p.at /* parametric position */}
+            (descr "<b>WS-TASV</B><BR>SMT Tact Switch 6.0x3.8 mm,2 pins")
+            (fp_line (start -3.0 1.95) (end 3.0 1.95) (layer F.Fab) (width 0.1))
+            (fp_line (start 3.0 1.95) (end 3.0 -1.95) (layer F.Fab) (width 0.1))
+            (fp_line (start 3.0 -1.95) (end -3.0 -1.95) (layer F.Fab) (width 0.1))
+            (fp_line (start -3.0 -1.95) (end -3.0 1.95) (layer F.Fab) (width 0.1))
+            (fp_line (start -3.1 -1.1) (end -3.1 -2.05) (layer F.SilkS) (width 0.2))
+            (fp_line (start -3.1 -2.05) (end 3.1 -2.05) (layer F.SilkS) (width 0.2))
+            (fp_line (start 3.1 -2.05) (end 3.1 -1.1) (layer F.SilkS) (width 0.2))
+            (fp_line (start -3.1 1.1) (end -3.1 2.05) (layer F.SilkS) (width 0.2))
+            (fp_line (start -3.1 2.05) (end 3.1 2.05) (layer F.SilkS) (width 0.2))
+            (fp_line (start 3.1 2.05) (end 3.1 1.1) (layer F.SilkS) (width 0.2))
+            (fp_poly
+                (pts
+                (xy -4.2 -2.25)
+                (xy 4.2 -2.25)
+                (xy 4.2 2.25)
+                (xy -4.2 2.25)
+                ) (layer F.CrtYd) (width 0.1)
+            )
+            (fp_circle (center -2.6 -1.2) (end -2.5 -1.2) (layer F.SilkS) (width 0.2))
+        `
+        function pins(def_neg, def_pos, def_side) {
+            return `
+                ${''/* pins */}
+                    (pad 1 smd rect (at ${def_neg}3.25 0.0) (size 1.5 1.4) (layers ${def_side}.Cu ${def_side}.Mask ${def_side}.Paste) ${p.r1.str})
+                    (pad 2 smd rect (at ${def_pos}3.25 0.0) (size 1.5 1.4) (layers ${def_side}.Cu ${def_side}.Mask ${def_side}.Paste) ${p.r2.str})
+            `
+        }
+        if (p.reverse) {
+            return `
+                ${standard}
+                ${pins('-', '', 'B')}
+                ${pins('', '-', 'F')})
+            `
+        } else {
+            return `
+                ${standard}
+                ${pins('-', '', 'B')})
+            `
+        }
+    }
+}
+
