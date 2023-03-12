eb22668cb2ef0385e20296a5c77b63190d39855c
Joey <joe@joey.codes>
2023-03-11 16:24:55 -0800

    make silk layer reveseable

--- footprints/wuerth_434121025816.js
+++ footprints/wuerth_434121025816.js
@@ -14,12 +14,6 @@ module.exports = {
             (fp_line (start 3.0 1.95) (end 3.0 -1.95) (layer F.Fab) (width 0.1))
             (fp_line (start 3.0 -1.95) (end -3.0 -1.95) (layer F.Fab) (width 0.1))
             (fp_line (start -3.0 -1.95) (end -3.0 1.95) (layer F.Fab) (width 0.1))
-            (fp_line (start -3.1 -1.1) (end -3.1 -2.05) (layer F.SilkS) (width 0.2))
-            (fp_line (start -3.1 -2.05) (end 3.1 -2.05) (layer F.SilkS) (width 0.2))
-            (fp_line (start 3.1 -2.05) (end 3.1 -1.1) (layer F.SilkS) (width 0.2))
-            (fp_line (start -3.1 1.1) (end -3.1 2.05) (layer F.SilkS) (width 0.2))
-            (fp_line (start -3.1 2.05) (end 3.1 2.05) (layer F.SilkS) (width 0.2))
-            (fp_line (start 3.1 2.05) (end 3.1 1.1) (layer F.SilkS) (width 0.2))
             (fp_poly
                 (pts
                 (xy -4.2 -2.25)
@@ -28,7 +22,6 @@ module.exports = {
                 (xy -4.2 2.25)
                 ) (layer F.CrtYd) (width 0.1)
             )
-            (fp_circle (center -2.6 -1.2) (end -2.5 -1.2) (layer F.SilkS) (width 0.2))
         `
         function pins(def_neg, def_pos, def_side) {
             return `
@@ -37,16 +30,31 @@ module.exports = {
                     (pad 2 smd rect (at ${def_pos}3.25 0.0) (size 1.5 1.4) (layers ${def_side}.Cu ${def_side}.Mask ${def_side}.Paste) ${p.r2.str})
             `
         }
+        function silk(def_side) {
+            const negative = def_side === 'F' ? '-' : ''
+            return `
+                (fp_line (start -3.1 -1.1) (end -3.1 -2.05) (layer ${def_side}.SilkS) (width 0.2))
+                (fp_line (start -3.1 -2.05) (end 3.1 -2.05) (layer ${def_side}.SilkS) (width 0.2))
+                (fp_line (start 3.1 -2.05) (end 3.1 -1.1) (layer ${def_side}.SilkS) (width 0.2))
+                (fp_line (start -3.1 1.1) (end -3.1 2.05) (layer ${def_side}.SilkS) (width 0.2))
+                (fp_line (start -3.1 2.05) (end 3.1 2.05) (layer ${def_side}.SilkS) (width 0.2))
+                (fp_line (start 3.1 2.05) (end 3.1 1.1) (layer ${def_side}.SilkS) (width 0.2))
+                (fp_circle (center ${negative}2.6 ${negative}1.2) (end ${negative}2.5 ${negative}1.2) (layer ${def_side}.SilkS) (width 0.2))
+            `
+        }
         if (p.reverse) {
             return `
                 ${standard}
                 ${pins('-', '', 'B')}
-                ${pins('', '-', 'F')})
-            `
-        } else {
-            return `
+                ${silk('B')}
+                ${pins('', '-', 'F')}
+                ${silk('F')})
+                `
+            } else {
+                return `
                 ${standard}
-                ${pins('-', '', 'B')})
+                ${pins('-', '', 'B')}
+                ${silk('B')})
             `
         }
     }
